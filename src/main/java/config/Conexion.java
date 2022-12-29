0dc4082aa0debee73c783e296f79e4a023a7654f
Soledad Adorno <gabriela.juj@gmail.com>
2022-12-28 23:53:15 -0300

    primer commit

---
+++ src/main/java/config/Conexion.java
@@ -0,0 +1,64 @@
+
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Conexion 
+{
+    public String driver="com.mysql.jdbc.Driver";
+    
+    
+    public Connection getConnection()
+    {
+        Connection conexion=null;
+        
+        try
+        {
+            Class.forName(driver);
+            conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/proyectocrud22548","root","");
+  
+        }
+        catch(ClassNotFoundException|SQLException e)
+        {
+            System.out.println(e);
+        }       
+        return conexion;
+    }
+    
+//    este metodo main es solo para probar la base de datos y ver que funcione
+            
+    public static void main(String[] args) throws SQLException 
+    {
+        Connection conexion=null;
+        Conexion con=new Conexion();
+        conexion=con.getConnection();
+        
+        
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        ps=conexion.prepareStatement("select * from socios");
+        
+        rs=ps.executeQuery();
+        
+        while(rs.next())
+        {
+            int id=rs.getInt("idSocio");
+            String nom=rs.getString("nombre");
+            String ape=rs.getString("apellido");
+            String dire=rs.getString("direccion");
+            boolean estado =rs.getBoolean("activo");
+            
+            System.out.println("ID: "+id+", Nombre: "+nom+", Apellido: "+ape+", Direccion: "+dire+", Estado: "+estado);
+        }
+        
+    }    
+}
+/**
+ *
+ * @author Sol
+ */
